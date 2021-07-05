d092155eb0e909e559f7b53a8b6735b1a2b89a0d
marinakulsha <marinakulsha@gmail.com>
2021-07-05 13:23:44 +0300

    addAPIQueries

--- cypress/support/pageObjects/landingPage.js
+++ cypress/support/pageObjects/landingPage.js
@@ -13,31 +13,41 @@ class LandingPage {
         return cy.xpath('.//div[@class="assetsTableWrapper"]/div[@class="assetRow"]/div[contains(text(), "DOT")]').click();
     }
 
-  checkUserWalletDOT(balanceUI){
+
+
+  checkUserWalletKSM(balanceUI){
     const { api } = ApiService;
-    api.query.tokens.accounts("5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",{UnderlyingAsset: 'DOT'}).then((obj) =>{
-      let userWalletBalanceDOT = formatData(obj.free);
-      console.log(userWalletBalanceDOT);
-      //expect(userWalletBalanceDOT).to.contain(balanceUI);
+    api.query.tokens.accounts("5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",{UnderlyingAsset: 'KSM'}).then((obj) =>{
+      let walletBalanceUI = parseFloat(balanceUI);
+      let userWalletBalance_API = formatData(obj.free);
+      console.log(userWalletBalance_API);
+      expect(userWalletBalance_API).to.contain(walletBalanceUI);
+      return userWalletBalance_API;
     })
   }
 
   checkUserSuppliedDOT(totalSuppliedDOT){
     const { api } = ApiService;
     api.rpc.controller.getUserUnderlyingBalancePerAsset("5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",{UnderlyingAsset: 'DOT'}).then((obj) =>{
-      let userSuppliedDOT = formatData(obj.free);
+      let totalSuppliedUI = parseFloat(totalSuppliedDOT);
+      let userSuppliedDOT = formatData(obj.amount);
       console.log(userSuppliedDOT);
-      expect(userSuppliedDOT).to.contain(totalSuppliedDOT);
+     //expect(userSuppliedDOT).to.contain(totalSuppliedUI);
     })
   }
 
   checkUserBorrowedDOT(totalBorrowedDOT){
     const { api } = ApiService;
     api.rpc.controller.getUserBorrowPerAsset("5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",{UnderlyingAsset: 'DOT'}).then((obj) =>{
-      let userBorrowedDOT = formatData(obj.free);
+      let totalBorrowedUI = parseFloat(totalBorrowedDOT);
+      let userBorrowedDOT = formatData(obj.amount);
       console.log(userBorrowedDOT);
-      expect(userBorrowedDOT).to.contain(totalBorrowedDOT);
+     // expect(userBorrowedDOT).to.contain(totalBorrowedUI);
     })
+  }
+
+  clickKSMAsset(){
+      return cy.xpath('.//div[@class="assetsTableWrapper"]/div[@class="assetRow"]/div[contains(text(), "KSM")]').click();
   }
 
 }
